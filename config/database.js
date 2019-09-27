9871346d30dcd93666c7bb763cc27c52919fa391
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-09-28 01:01:00 +0600

    db change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
  module.exports = {
- 	mongoURI: 'mongodb+srv://grocery_shop:<password>@cluster0-rrhom.mongodb.net/test?retryWrites=true&w=majority',
+ 	mongoURI: 'mongodb+srv://grocery_shop:Mahmudul5809@cluster0-rrhom.mongodb.net/test?retryWrites=true&w=majority',
  }
 }else{
   module.exports = {
