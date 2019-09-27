c3689d4aef1ca142135dc46f8a9b16ee5d0c2b7a
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-09-28 02:09:15 +0600

    config

--- config/secret.js
+++ config/secret.js
@@ -1,12 +1,12 @@
 module.exports = {
 	auth:{
-		user: 'mahmudul15-5809@diu.edu.bd',
-		pass: 'puanffeswvyrlzed'
+		user: 'Your Email',
+		pass: 'Your Password'
 	},
-	googleClientID: '130272441787-agnuhuehdbn9tmbcukjkg3ii5v4qfjnn.apps.googleusercontent.com',
-	googleClientSecret: 'tOo32izKzFEDg7rRghAGI0IX',
+	googleClientID: 'XXXXX',
+	googleClientSecret: 'XXXX',
 	facebook : {
-	  	appID : '525198641639231',
-	  	secretId: 'df2b00b584a0d6763a2e5bcddffa635d'
+	  	appID : 'XXXX',
+	  	secretId: 'XXXX'
 	},
 }
